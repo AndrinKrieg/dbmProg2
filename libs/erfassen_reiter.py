57f742de093e1e6dc158a1f8f6b54a1bf0af7209
Andrin <55581326+AndrinKrieg@users.noreply.github.com>
2019-12-11 22:26:05 +0100

    names
    Namen in Dic erstellt

--- libs/erfassen_reiter.py
+++ libs/erfassen_reiter.py
@@ -1,9 +1,9 @@
 import json
 
-def termin_speichern(category, grad, name, vorname, jahrgang, adresse):
+def reiter_erfassen(grad, name, vorname, jahrgang, adresse, plz, ort, tel, liz_brev, namepf, passnummer, geschlecht, farbe, alter, rasse, gwp, pruefung1, pruefung2, pruefung3, pruefung4, pruefung5, pruefung6, pruefung7, pruefung8, pruefung9, pruefung10, pruefung11, pruefung12, namepf2, passnummer2, geschlecht2, farbe2, alter2, rasse2, gwp2, pruefung1pf2, pruefung2pf2, pruefung3pf2, pruefung4pf2, pruefung5pf2, pruefung6pf2, pruefung7pf2, pruefung8pf2, pruefung9pf2, pruefung10pf2, pruefung11pf2, pruefung12pf2,):
     
     json_daten = load_json()
-    alle_termine = json_daten.get("termine", {})
+    alle_anmeldungen = json_daten.get("reiter", {})
         
     reiter = {
         "grad": grad,
@@ -11,11 +11,53 @@ def termin_speichern(category, grad, name, vorname, jahrgang, adresse):
         "vorname": vorname,
         "jahrgang": jahrgang,
         "adresse": adresse,
+        "plz": plz,
+        "ort": ort,
+        "tel": tel,
+        "liz_brev": liz_brev,
+        "namepf": namepf,
+        "passnummer": passnummer,
+        "geschlecht": geschlecht,
+        "farbe": farbe,
+        "alter": alter,
+        "rasse": rasse,
+        "gwp": gewinnpunkte,
+        "pruefung1": pruefung1,
+        "pruefung2": pruefung2,
+        "pruefung3": pruefung3,
+        "pruefung4": pruefung4,
+        "pruefung5": pruefung5,
+        "pruefung6": pruefung6,
+        "pruefung7": pruefung7,
+        "pruefung8": pruefung8,
+        "pruefung9": pruefung9,
+        "pruefung10": pruefung10,
+        "pruefung11": pruefung11,
+        "pruefung12": pruefung12,
+        "namepf2": namepf2,
+        "passnummer2": passnummer2,
+        "geschlecht2": geschlecht2,
+        "farbe2": farbe2,
+        "alter2": alter2,
+        "rasse2": rasse2,
+        "gwp2": gewinnpunkte2,
+        "pruefung1pf2": pruefung1pf2,
+        "pruefung2pf2": pruefung2pf2,
+        "pruefung3pf2": pruefung3pf2,
+        "pruefung4pf2": pruefung4pf2,
+        "pruefung5pf2": pruefung5pf2,
+        "pruefung6pf2": pruefung6pf2,
+        "pruefung7pf2": pruefung7pf2,
+        "pruefung8pf2": pruefung8pf2,
+        "pruefung9pf2": pruefung9pf2,
+        "pruefung10pf2": pruefung10pf2,
+        "pruefung11pf2": pruefung11pf2,
+        "pruefung12pf2": pruefung12pf2,
     }
         
-    alle_termine[category] = termin
+    alle_anmeldungen[grad] = anmeldung
         
-    json_daten["termine"] = alle_termine
+    json_daten["reiter"] = alle_anmeldungen
 
     save_to_json(json_daten)
     return json_daten                        #Json-Datei mit neuer Eingabe wird zurückgegeben
