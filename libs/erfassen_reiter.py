f37f3586690c8476728fb3bf404ad753201f5d69
Andrin <55581326+AndrinKrieg@users.noreply.github.com>
2020-01-11 15:16:19 +0100

    Offlineupdates Feiertage
    Ich hatte nur zwischendurch für Porg2 zeit, arbeitete an anderen DBM-Porjekten.

--- libs/erfassen_reiter.py
+++ libs/erfassen_reiter.py
@@ -1,11 +1,31 @@
 import json
 
-def reiter_erfassen(grad, name, vorname, jahrgang, adresse, plz, ort, tel, liz_brev, namepf, passnummer, geschlecht, farbe, alter, rasse, gwp, pruefung1, pruefung2, pruefung3, pruefung4, pruefung5, pruefung6, pruefung7, pruefung8, pruefung9, pruefung10, pruefung11, pruefung12):
-    
+#def reiter_erfassen(grad, name, vorname, jahrgang, adresse, plz, ort, tel, liz_brev, namepf, passnummer, geschlecht, farbe, alter, rasse, gwp, pruefung1, pruefung2, pruefung3, pruefung4, pruefung5, pruefung6, pruefung7, pruefung8, pruefung9, pruefung10, pruefung11, pruefung12):
+def reiter_erfassen(form_data):
     json_daten = load_json()
     alle_anmeldungen = json_daten.get("reiter", {})
-        
+
     einreiter = {
+        "grad": form_data['grad'],
+        "name": form_data['name'],
+        "vorname": form_data['vorname'],
+        "jahrgang": form_data['jahrgang'],
+        "adresse": form_data['adresse'],
+        "plz": form_data['plz'],
+        "ort": form_data['ort'],
+        "tel": form_data['tel'],
+        "liz_brev": form_data['liz_brev'],
+        "namepf": form_data['namepf'],
+        "passnummer": form_data['passnummer'],
+        "geschlecht": form_data['geschlecht'],
+        "farbe": form_data['farbe'],
+        "alter": form_data['alter'],
+        "rasse": form_data['rasse'],
+        "gwp": form_data['gwp'],
+        "pruefungen": form_data.getlist("pruefungen")
+    }
+
+    """einreiter = {
         "grad": grad,
         "name": name,
         "vorname": vorname,
@@ -17,42 +37,45 @@ def reiter_erfassen(grad, name, vorname, jahrgang, adresse, plz, ort, tel, liz_b
         "liz_brev": liz_brev,
         "namepf": namepf,
         "passnummer": passnummer,
-        "geschlecht": geschlecht, 
-        "farbe": farbe, 
-        "alter": alter, 
-        "rasse": rasse, 
-        "gwp": gwp, 
-        "pruefung1": pruefung1, 
-        "pruefung2": pruefung2, 
+        "geschlecht": geschlecht,
+        "farbe": farbe,
+        "alter": alter,
+        "rasse": rasse,
+        "gwp": gwp,
+        "pruefung1": pruefung1,
+        "pruefung2": pruefung2,
         "pruefung3": pruefung3,
-        "pruefung4": pruefung4, 
-        "pruefung5": pruefung5, 
-        "pruefung6": pruefung6, 
-        "pruefung7": pruefung7, 
-        "pruefung8": pruefung8, 
-        "pruefung9": pruefung9, 
-        "pruefung10": pruefung10, 
-        "pruefung11": pruefung11, 
+        "pruefung4": pruefung4,
+        "pruefung5": pruefung5,
+        "pruefung6": pruefung6,
+        "pruefung7": pruefung7,
+        "pruefung8": pruefung8,
+        "pruefung9": pruefung9,
+        "pruefung10": pruefung10,
+        "pruefung11": pruefung11,
         "pruefung12": pruefung12,
     }
-        
-    alle_anmeldungen[passnummer] = einreiter
-        
+
+    alle_anmeldungen[passnummer] = einreiter"""
+
+    alle_anmeldungen[form_data['passnummer']] = einreiter
     json_daten["reiter"] = alle_anmeldungen
 
     save_to_json(json_daten)
-    return json_daten                       
+    return json_daten
+
 
 def load_json():
     json_daten = {}
     try:
-        with open('data/data.json') as open_file:   
+        with open('data/data.json') as open_file:
             json_daten = json.load(open_file)
-    
-    except FileNotFoundError:                       
+
+    except FileNotFoundError:
         print("File not found")
 
     return json_daten
+
 
 def save_to_json(daten):
     with open('data/data.json', "w") as open_file:
--- main.py
+++ main.py
@@ -9,43 +9,19 @@ def startseite():
 0
 @app.route("/reitererfassen", methods=['GET', 'POST'])
 def reitererfassen():
-    if request.method == 'POST':
-        print(request.form)
-        grad = request.form['grad']
-        name = request.form['name']
-        vorname = request.form['vorname']
-        jahrgang = request.form['jahrgang']
-        adresse = request.form['adresse']
-        plz = request.form['plz']
-        ort = request.form['ort']
-        tel = request.form['tel']
-        liz_brev = request.form['liz_brev']
-        namepf = request.form['namepf']
-        passnummer = request.form['passnummer']
-        geschlecht = request.form['geschlecht']
-        farbe = request.form['farbe']
-        alter = request.form['alter']
-        rasse = request.form['rasse']
-        gwp = request.form['gwp']
-        pruefung1 = "ja" if len(request.form.getlist("pruefung1")) > 0 else "nein"
-        pruefung2 = "ja" if len(request.form.getlist("pruefung2")) > 0 else "nein"
-        pruefung3 = "ja" if len(request.form.getlist("pruefung3")) > 0 else "nein"
-        pruefung4 = "ja" if len(request.form.getlist("pruefung4")) > 0 else "nein"
-        pruefung5 = "ja" if len(request.form.getlist("pruefung5")) > 0 else "nein"
-        pruefung6 = "ja" if len(request.form.getlist("pruefung6")) > 0 else "nein"
-        pruefung7 = "ja" if len(request.form.getlist("pruefung7")) > 0 else "nein"
-        pruefung8 = "ja" if len(request.form.getlist("pruefung8")) > 0 else "nein"
-        pruefung9 = "ja" if len(request.form.getlist("pruefung9")) > 0 else "nein"
-        pruefung10 = "ja" if len(request.form.getlist("pruefung10")) > 0 else "nein"
-        pruefung11 = "ja" if len(request.form.getlist("pruefung11")) > 0 else "nein"
-        pruefung12 = "ja" if len(request.form.getlist("pruefung12")) > 0 else "nein"
-        returned_data = erfassen_reiter.reiter_erfassen(grad, name, vorname, jahrgang, adresse, plz, ort, tel, liz_brev, namepf, passnummer, geschlecht, farbe, alter, rasse, gwp, pruefung1, pruefung2, pruefung3, pruefung4, pruefung5, pruefung6, pruefung7, pruefung8, pruefung9, pruefung10, pruefung11, pruefung12)
+    if request.method == 'POST': 
+        returned_data = erfassen_reiter.reiter_erfassen(request.form)
+        return redirect(url_for('danke'))
     return render_template("reitererfassen.html")
 
 @app.route("/anmeldeliste")
 def anmeldeliste():
     reiter_daten = erfassen_reiter.load_json()
     return render_template("anmeldeliste.html", daten=reiter_daten)
+
+@app.route("/danke")
+def danke():
+    return render_template("danke.html")
     
 if __name__== "__main__":
     app.run(debug=True, port=5000)
